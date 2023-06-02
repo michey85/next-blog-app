cab2746aa8e255490cb6a0c00d820b925a1dcc99
Mikhail Nepomniashchii <michey85@gmail.com>
2023-06-02 11:27:19 +0200

    cleanup

---
+++ data/users.ts
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    id: "1",
+    email: "mishanep@gmail.com",
+    name: "Misha Nep",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "2",
+    email: "admin@pcgramota.ru",
+    name: "Super Admin",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "3",
+    email: "any@gmail.com",
+    name: "Just a Guest",
+    password: "12345",
+    role: "guest",
+  },
+];
